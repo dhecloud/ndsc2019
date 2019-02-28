03c6aef9ad75a70f78c521fc287faa3686c77dd8
dhecloud <andrewkohjinjie@hotmail.com>
2019-02-28 16:32:37 +0800

    minor

--- network.py
+++ network.py
@@ -6,27 +6,42 @@ import random
 import numpy as np
 import torchvision.models as models
 
+# resnet_dict= {'resnet18':[models.resnet152(pretrained=True), 512], 'resnet34':[models.resnet34(pretrained=True), 512], 'resnet50':[models.resnet50(pretrained=True), 2048], 'resnet101':[models.resnet101(pretrained=True), 2048], 'resnet152':[models.resnet152(pretrained=True), 2048]}
+
 class BERT(nn.Module):
     def __init__(self, opt, num_labels=58):
         super().__init__()
         
+        self.use_images = opt.images
+        self.no_bert= opt.no_bert
         self.num_labels= num_labels
-        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        if not self.no_bert:
+            self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.dropout = nn.Dropout(0.1)
-        self.resnet = torch.nn.Sequential(*(list(models.resnet152(pretrained=True).children())[:-1]))
-        self.resnet_downsample = torch.nn.Linear(2048, opt.last_layer_size)
-        self.classifier = torch.nn.Linear(768+opt.last_layer_size, num_labels)
+        if self.use_images:
+            self.resnet = torch.nn.Sequential(*(list(models.resnet152(pretrained=True).children())[:-1]))
+            self.resnet_downsample = torch.nn.Linear(2048, opt.last_layer_size)
+        if self.no_bert:
+            self.classifier = torch.nn.Linear(opt.last_layer_size, num_labels)
+        elif self.use_images:
+            self.classifier = torch.nn.Linear(768+opt.last_layer_size, num_labels)
+        else:
+            self.test = torch.nn.Linear(768, num_labels)
 
 
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None, image=None):
-        _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-        if image is not None:
+    def forward(self, input_ids=None, token_type_ids=None, attention_mask=None, labels=None, image=None):
+        if not self.no_bert:
+            _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        if self.use_images:
             img = self.resnet(image)
             img = img.view(img.size(0), -1)
             img = self.resnet_downsample(img)
-            assert pooled_output.shape[0] == img.shape[0]
-            assert len(pooled_output.shape) == len(img.shape)
-            pooled_output = torch.cat((pooled_output, img), 1)
+            if not self.no_bert:
+                assert pooled_output.shape[0] == img.shape[0]
+                assert len(pooled_output.shape) == len(img.shape)
+                pooled_output = torch.cat((pooled_output, img), 1)
+            else:
+                pooled_output = img
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         
